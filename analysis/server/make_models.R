98e4a08234459b1f4cdae6c1cd96f70bd889784a
DominiqueMakowski <dom.mak19@gmail.com>
2026-08-23 12:47:30 +0100

    edits to model specs

--- analysis/server/make_models.R
+++ analysis/server/make_models.R
@@ -5,12 +5,13 @@
 
 library(brms)
 library(cogmod) # remotes::install_github("DominiqueMakowski/cogmod")
+library(dplyr)
 
 task_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID", unset = "1"))
 total_cores <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", unset = "2"))
 
 chains_per_node <- 2
-threads_per_chain <- total_cores / chains_per_node  # 16 cores / 2 chains = 8 threads per chain
+threads_per_chain <- total_cores / chains_per_node # 16 cores / 2 chains = 8 threads per chain
 
 warmup <- 1000
 iter <- warmup + 200
@@ -22,109 +23,150 @@ df <- rbind(
   read.csv("https://raw.githubusercontent.com/RealityBending/IllusionGameComputational/refs/heads/main/data/illusion_part3.csv")
 )
 df$Illusion_Difference <- abs(df$Illusion_Difference)
+df$Illusion_Effect <- factor(ifelse(df$Illusion_Strength >= 0, "Conflicting", "Facilitating"), levels = c("Conflicting", "Facilitating"))
 
-df <- df[1:10001, ] # TODO: remove this when running full models on the cluster.
+# Normalize predictors
+# summarize(df,
+#   MinDiff = min(Illusion_Difference),
+#   MaxDiff = max(Illusion_Difference),
+#   MinStrength = min(Illusion_Strength),
+#   MinStrength_abs = min(abs(Illusion_Strength)),
+#   MaxStrength = max(abs(Illusion_Strength)),
+#   .by = c("Illusion_Effect", "Illusion_Type")
+# )
+df <- mutate(df,
+  Illusion_DifferenceZ = as.numeric(datawizard::normalize(Illusion_Difference)),
+  Illusion_StrengthZ = sign(Illusion_Strength) * as.numeric(datawizard::normalize(abs(Illusion_Strength))),
+  .by = "Illusion_Type"
+)
+# plot(df$Illusion_Difference, df$Illusion_DifferenceZ, col = as.numeric(as.factor(df$Illusion_Type)), pch = 19, cex = 0.5)
+# plot(df$Illusion_Strength, df$Illusion_StrengthZ, col = as.numeric(as.factor(df$Illusion_Type)), pch = 19, cex = 0.5)
 
+
+# TODO: data subset (to be removed in the final version)
+df <- df[
+  df$Participant %in% unique(df$Participant)[1:30],
+]
+
+# =========================================================================
+# GAMs --------------------------------------------------------------------
+# =========================================================================
 
 # LNR ---------------------------------------------------------------------
 
 # Formula
 f <- bf(
-  RT | dec(Error) ~ poly(Illusion_Difference, 2) * poly(Illusion_Strength, 2),
-  nuone ~ poly(Illusion_Difference, 2) * poly(Illusion_Strength, 2),
-  sigmazero ~ poly(Illusion_Difference, 2) * poly(Illusion_Strength, 2),
-  sigmaone ~ poly(Illusion_Difference, 2) * poly(Illusion_Strength, 2),
-  tau ~ Illusion_Difference + Illusion_Strength,
-  minrt = min(df$RT),
-  family = lnr()
+  RT | dec(Error) ~ t2(Illusion_DifferenceZ, Illusion_StrengthZ,
+    k = c(5, 5),
+    bs = c("cr", "cr")
+  ),
+  nuone ~ t2(Illusion_DifferenceZ, Illusion_StrengthZ,
+    k = c(5, 5),
+    bs = c("cr", "cr")
+  ),
+  sigmazero ~ t2(Illusion_DifferenceZ, Illusion_StrengthZ,
+    k = c(5, 5),
+    bs = c("cr", "cr")
+  ),
+  sigmaone ~ t2(Illusion_DifferenceZ, Illusion_StrengthZ,
+    k = c(5, 5),
+    bs = c("cr", "cr")
+  ),
+  ndt ~ t2(Illusion_DifferenceZ, Illusion_StrengthZ,
+    k = c(5, 5),
+    bs = c("cr", "cr")
+  ),
+  poutlier ~ 1,
+  family = cogmod_lnr()
 )
 
 
-# Weakly informative priors
+# Informative priors
 # brms::get_prior(f, data = df)
 priors <- c(
-  brms::set_prior("normal(0.3, 1)", class = "Intercept", dpar = ""),
-  brms::set_prior("normal(0, 1)", class = "Intercept", dpar = "nuone"),
-  brms::set_prior("normal(-0.5, 0.5)", class = "Intercept", dpar = "sigmazero"),
-  brms::set_prior("normal(0, 0.5)", class = "Intercept", dpar = "sigmaone"),
-  brms::set_prior("normal(3, 10)", class = "Intercept", dpar = "tau")
+  cogmod_priors(f, df[df$Illusion_Type == "MullerLyer", ]),
+  brms::prior("normal(0, 1)", class = "b", dpar = "", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "nuone", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "sigmazero", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "sigmaone", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "ndt", coef = ""),
+  replace = TRUE
 )
-for (p in c(
-  "polyIllusion_Difference21", "polyIllusion_Strength21",
-  "polyIllusion_Difference22", "polyIllusion_Strength22",
-  "polyIllusion_Difference21:polyIllusion_Strength21", "polyIllusion_Difference21:polyIllusion_Strength22",
-  "polyIllusion_Difference22:polyIllusion_Strength21", "polyIllusion_Difference22:polyIllusion_Strength22"
-)) {
-  for (dpar in c("", "nuone", "sigmazero", "sigmaone")) {
-    if (grepl(":", p)) { # Interaction
-      prior <- "normal(0, 1)"
-    } else if (grepl("22", p)) {
-      prior <- "normal(0, 5)"
-    } else if (grepl("21", p)) {
-      prior <- "normal(0, 10)"
-    } else {
-      prior <- "normal(0, 5)"
-    }
-    priors <- c(priors, brms::set_prior(prior, class = "b", dpar = dpar, coef = p))
-  }
-}
-priors <- c(priors, brms::set_prior("normal(0, 1)", class = "b", dpar = "tau", coef = c("Illusion_Difference", "Illusion_Strength")))
-priors <- brms::validate_prior(priors, f, data = df)
 
 
 # Fit
-m_lnr_muller <- brm(f,
-  data = df[df$Illusion_Type == "MullerLyer", ], # muller-lyer only? maybe add 1-2 other types? ebbinghaus and vertical-horizontal
-  init = 0,
+gam_lnr_muller <- brm(f,
+  data = df[df$Illusion_Type == "MullerLyer", ],
   prior = priors,
-  family = lnr(),
-  stanvars = lnr_stanvars(),
+  init = cogmod_inits(f, df[df$Illusion_Type == "MullerLyer", ]),
+  stanvars = cogmod_stanvars(f),
+  backend = "cmdstanr",
   warmup = warmup,
   iter = iter,
-  chains = chains_per_node,
-  cores = chains_per_node,
-  threads = threading(threads_per_chain),
-  backend = "cmdstanr",
-  save_pars = save_pars(all = TRUE),
-  algorithm = "sampling" #algorithm = "pathfinder", max_lbfgs_iters = 6000, draws = 4000
+  algorithm = "pathfinder", chains = 16, single_path_draws = 4000, max_lbfgs_iters = 8000, threads = 8
+  # chains = chains_per_node,
+  # cores = chains_per_node,
+  # threads = threading(threads_per_chain),
+  # save_pars = save_pars(all = TRUE),
+  # algorithm = "sampling"
 )
 
 # TODO: WAIC instead of loo will probably be faster.
 # m_lnr_muller <- brms::add_criterion(m_lnr_muller, "loo", moment_match = FALSE)
 # m_lnr_muller <- brms::add_criterion(m_lnr_muller, "waic")
 
-dir.create("models", showWarnings = FALSE)
 
-saveRDS(m_lnr_muller, sprintf("models/m_lnr_muller_%d.rds", task_id))
+saveRDS(gam_lnr_muller, sprintf("../models/gam_lnr_muller_%d.rds", task_id))
+
+# =========================================================================
+# LINEAR ------------------------------------------------------------------
+# =========================================================================
+
+# Formula
+f <- bf(
+  RT | dec(Error) ~ Illusion_Effect * Illusion_DifferenceZ * abs(Illusion_StrengthZ),
+  nuone ~ Illusion_Effect * Illusion_DifferenceZ * abs(Illusion_StrengthZ),
+  sigmazero ~ Illusion_Effect * Illusion_DifferenceZ * abs(Illusion_StrengthZ),
+  sigmaone ~ Illusion_Effect * Illusion_DifferenceZ * abs(Illusion_StrengthZ),
+  ndt ~ Illusion_Effect * Illusion_DifferenceZ * abs(Illusion_StrengthZ),
+  poutlier ~ 1,
+  family = cogmod_lnr()
+)
 
 
-
-# BayesFlow-Assisted Process ----------------------------------------------
-
-
-# # Formula
-# f <- bf(
-#   RT | dec(Error) ~ Illusion_Difference,
-#   nuone ~ Illusion_Difference,
-#   sigmazero ~ Illusion_Difference,
-#   sigmaone ~ Illusion_Difference,
-#   tau ~ Illusion_Difference,
-#   minrt = min(df$RT),
-#   family = lnr()
-# )
-#
-# # Fit
-# m_lnr_muller <- brm(f,
-#   data = df[df$Illusion_Type == "MullerLyer", ],
-#   init = 0,
-#   family = lnr(),
-#   stanvars = lnr_stanvars(),
-#   iter = 4000,
-#   chains = 2, threads = threading(2),
-#   backend = "cmdstanr",
-#   save_pars = save_pars(all = TRUE),
-#   algorithm = "pathfinder", max_lbfgs_iters = 500, draws = 100
-# )
-#
-# insight::find_parameters(m_lnr_muller, effects = "all", component = "all")
+# Informative priors
 # brms::get_prior(f, data = df)
+priors <- c(
+  cogmod_priors(f, df[df$Illusion_Type == "MullerLyer", ]),
+  brms::prior("normal(0, 1)", class = "b", dpar = "", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "nuone", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "sigmazero", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "sigmaone", coef = ""),
+  brms::prior("normal(0, 1)", class = "b", dpar = "ndt", coef = ""),
+  replace = TRUE
+)
+
+
+# Fit
+m1_lnr_muller <- brm(f,
+  data = df[df$Illusion_Type == "MullerLyer", ],
+  prior = priors,
+  init = cogmod_inits(f, df[df$Illusion_Type == "MullerLyer", ]),
+  stanvars = cogmod_stanvars(f),
+  backend = "cmdstanr",
+  warmup = warmup,
+  iter = iter,
+  algorithm = "pathfinder", chains = 16, single_path_draws = 4000, max_lbfgs_iters = 8000, threads = 8
+  # chains = chains_per_node,
+  # cores = chains_per_node,
+  # threads = threading(threads_per_chain),
+  # save_pars = save_pars(all = TRUE),
+  # algorithm = "sampling"
+)
+
+# TODO: WAIC instead of loo will probably be faster.
+# m_lnr_muller <- brms::add_criterion(m_lnr_muller, "loo", moment_match = FALSE)
+# m_lnr_muller <- brms::add_criterion(m_lnr_muller, "waic")
+
+
+saveRDS(m1_lnr_muller, sprintf("../models/m1_lnr_muller_%d.rds", task_id))
